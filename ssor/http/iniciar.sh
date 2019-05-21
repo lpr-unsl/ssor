f1a44d1cfb2d50a98b73d9d52973ab9b5f515db6
carlostaffe <carlos.taffe@gmail.com>
2019-05-21 17:49:25 +0000

    manejo version lpr ..falta..

--- ssor/http/iniciar.sh
+++ ssor/http/iniciar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+version=`cat ../../version.txt`
 puente=`brctl show | egrep lan1`
 if [ -z "$puente" ]
 then
@@ -51,14 +51,14 @@ fi
 
 
 
-docker run --detach --hostname latoma -it --name latoma --cap-add NET_ADMIN --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" cliente:2.0 bash
-docker run --detach --hostname merlo -it --name merlo --cap-add NET_ADMIN cliente-cli:2.0 bash
-docker run --detach --hostname potrero -it --name potrero --cap-add NET_ADMIN router:2.0 bash
-docker run --detach --hostname laflorida -it --name laflorida --cap-add NET_ADMIN router:2.0 bash
-docker run --detach --hostname desaguadero -it --name desaguadero --cap-add NET_ADMIN --privileged servidor:2.0 bash
-docker run --detach --hostname nogoli -it --name nogoli --cap-add NET_ADMIN --privileged servidor:2.0 bash
-docker run --detach --hostname carrizal -it --name carrizal --cap-add NET_ADMIN router:2.0 bash
-docker run --detach --hostname laslenias -it --name laslenias --cap-add NET_ADMIN --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" cliente:2.0 bash
+docker run --detach --hostname latoma -it --name latoma --cap-add NET_ADMIN --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" cliente:$version bash
+docker run --detach --hostname merlo -it --name merlo --cap-add NET_ADMIN cliente-cli:$version bash
+docker run --detach --hostname potrero -it --name potrero --cap-add NET_ADMIN router:$version bash
+docker run --detach --hostname laflorida -it --name laflorida --cap-add NET_ADMIN router:$version bash
+docker run --detach --hostname desaguadero -it --name desaguadero --cap-add NET_ADMIN --privileged servidor:$version bash
+docker run --detach --hostname nogoli -it --name nogoli --cap-add NET_ADMIN --privileged servidor:$version bash
+docker run --detach --hostname carrizal -it --name carrizal --cap-add NET_ADMIN router:$version bash
+docker run --detach --hostname laslenias -it --name laslenias --cap-add NET_ADMIN --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" cliente:$version bash
 
 docker exec -it latoma ip ro del default
 docker exec -it merlo ip ro del default
