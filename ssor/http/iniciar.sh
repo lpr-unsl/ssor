0b6ab54b7cd905db55f8f3e6d92f3a12ec131a58
carlost <carlos.taffe@gmail.com>
2024-08-11 11:12:41 -0300

    Probado inicio de http, falta revisar los otros scripts

--- ssor/http/iniciar.sh
+++ ssor/http/iniciar.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
-version=`cat root/Documents/version.txt`
-puente=`brctl show | egrep lan1`
+#version=`cat ../../version.txt`
+version=2.0
+puente=`docker network list | egrep lan1`
 if [ -z "$puente" ]
 then
-	brctl addbr lan1
-	brctl addbr lan2
-	brctl addbr lan3
-	brctl addbr ppp1
-	brctl addbr ppp2
-	brctl addbr man1
+	docker network create -d bridge lan1 --subnet 192.168.1.0/24 --gateway 192.168.1.254 --label lan1 --opt com.docker.network.bridge.name=lan1 --opt com.docker.network.container_iface_prefix=lan1
+	docker network create -d bridge lan2 --subnet 172.16.4.0/23 --gateway 172.16.4.254 --label lan2 --opt com.docker.network.bridge.name=lan2 --opt com.docker.network.container_iface_prefix=lan2
+	docker network create -d bridge lan3 --subnet 10.22.0.0/16 --gateway 10.22.255.254 --label lan3 --opt com.docker.network.bridge.name=lan3 --opt com.docker.network.container_iface_prefix=lan3
+	docker network create -d bridge ppp1 --subnet 200.8.4.16/29 --gateway 200.8.4.22 --label ppp1 --opt com.docker.network.bridge.name=ppp1 --opt com.docker.network.container_iface_prefix=ppp1
+	docker network create -d bridge ppp2 --subnet 170.0.2.0/29 --gateway 170.0.2.1 --label ppp2 --opt com.docker.network.bridge.name=ppp2 --opt com.docker.network.container_iface_prefix=ppp2
+	docker network create -d bridge man1 --subnet 8.8.8.0/28 --gateway 8.8.8.2 --label man1 --opt com.docker.network.bridge.name=man1 --opt com.docker.network.container_iface_prefix=man1
 fi
 
-ip link set dev lan1 up
-ip link set dev lan2 up
-ip link set dev lan3 up
-ip link set dev ppp1 up
-ip link set dev ppp2 up
-ip link set dev man1 up
-
-montaje=`df -h | egrep docker`
-if [ -z "$montaje" ]
-then
-	service docker stop
-	mount $1 /var/lib/docker
-	service docker start
-fi
-/sbin/iptables -P FORWARD ACCEPT
 contenedores=`docker ps -aq|wc -l`
 
 if [ $contenedores -gt 0 ]
@@ -47,16 +33,46 @@ then
 	docker rmi http-laslenias
 fi
 
+docker create --network=bridge --hostname latoma --name latoma -it --cap-add NET_ADMIN --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" cliente:$version
+docker create --network=bridge --hostname merlo --name merlo -it --cap-add NET_ADMIN cliente-cli:$version
+docker create --network=bridge --hostname potrero --name potrero -it --cap-add NET_ADMIN router:$version
+docker create --network=bridge --hostname laflorida --name laflorida -it --cap-add NET_ADMIN router:$version
+docker create --network=bridge --hostname desaguadero --name desaguadero -it --cap-add NET_ADMIN --privileged servidor:$version
+docker create --network=bridge --hostname nogoli --name nogoli -it --cap-add NET_ADMIN --privileged servidor:$version
+docker create --network=bridge --hostname carrizal --name carrizal -it --cap-add NET_ADMIN router:$version
+docker create --network=bridge --hostname laslenias --name laslenias -it --cap-add NET_ADMIN --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" cliente:$version
+#
+docker network connect lan1 potrero --ip 192.168.1.1
+docker network connect lan1 merlo --ip 192.168.1.48
+docker network connect lan2 potrero --ip 172.16.4.1
+docker network connect lan2 latoma --ip 172.16.4.10
+docker network connect ppp1 potrero --ip 200.8.4.18
+docker network connect ppp1 laflorida --ip 200.8.4.17
+docker network connect ppp1 potrero --ip 200.8.4.18
+docker network connect man1 laflorida --ip 8.8.8.14
+docker network connect man1 nogoli --ip 8.8.8.8
+docker network connect man1 desaguadero --ip 8.8.8.1
+docker network connect ppp2 desaguadero --ip 170.0.2.6
+docker network connect ppp2 carrizal --ip 170.0.2.5
+docker network connect lan3 carrizal --ip 10.22.0.1
+docker network connect lan3 laslenias --ip 10.22.0.150
 
+xterm -T "latoma" -fa monaco -fs 11 -e "docker start -ia latoma" &
+xterm -T "merlo" -fa monaco -fs 11 -e "docker start -ia merlo" &
+xterm -T "potrero" -fa monaco -fs 11 -e "docker start -ia potrero" &
+xterm -T "laflorida" -fa monaco -fs 11 -e "docker start -ia laflorida" &
+xterm -T "desaguadero" -fa monaco -fs 11 -e "docker start -ia desaguadero" &
+xterm -T "nogoli" -fa monaco -fs 11 -e "docker start -ia nogoli" &
+xterm -T "carrizal" -fa monaco -fs 11 -e "docker start -ia carrizal" &
+xterm -T "laslenias" -fa monaco -fs 11 -e "docker start -ia laslenias" &
 
-docker run --detach --hostname latoma -it --name latoma --cap-add NET_ADMIN --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" cliente:$version bash
-docker run --detach --hostname merlo -it --name merlo --cap-add NET_ADMIN cliente-cli:$version bash
-docker run --detach --hostname potrero -it --name potrero --cap-add NET_ADMIN router:$version bash
-docker run --detach --hostname laflorida -it --name laflorida --cap-add NET_ADMIN router:$version bash
-docker run --detach --hostname desaguadero -it --name desaguadero --cap-add NET_ADMIN --privileged servidor:$version bash
-docker run --detach --hostname nogoli -it --name nogoli --cap-add NET_ADMIN --privileged servidor:$version bash
-docker run --detach --hostname carrizal -it --name carrizal --cap-add NET_ADMIN router:$version bash
-docker run --detach --hostname laslenias -it --name laslenias --cap-add NET_ADMIN --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" cliente:$version bash
+not_running=`docker ps -a | egrep Created`
+while [ -n "$not_running" ]
+do
+	sleep 1
+	#echo Waiting until all containers are running
+	not_running=`docker ps -a | egrep Created`
+done
 
 docker exec -it latoma ip ro del default
 docker exec -it merlo ip ro del default
@@ -66,35 +82,6 @@ docker exec -it desaguadero ip ro del default
 docker exec -it nogoli ip ro del default
 docker exec -it carrizal ip ro del default
 docker exec -it laslenias ip ro del default
-
-#pipework lan2 -i lan2 latoma 0.0.0.0/24
-#pipework lan1 -i lan1 merlo  0.0.0.0/24
-#pipework lan1 -i lan1 potrero 0.0.0.0/24
-#pipework lan2 -i lan2 potrero 0.0.0.0/24
-#pipework ppp1 -i ppp1 potrero 0.0.0.0/24
-#pipework ppp1 -i ppp1 laflorida 0.0.0.0/24
-#pipework man1 -i man1 laflorida 0.0.0.0/24
-#pipework man1 -i man1 nogoli 0.0.0.0/24
-#pipework man1 -i man1 desaguadero 0.0.0.0/24
-#pipework ppp1 -i ppp1 desaguadero 0.0.0.0/24
-#pipework ppp1 -i ppp1 carrizal 0.0.0.0/24
-#pipework lan3 -i lan3 carrizal 0.0.0.0/24
-#pipework lan3 -i lan3 laslenias 0.0.0.0/24
-
-pipework lan2 -i lan2 latoma 172.16.4.10/23
-pipework lan1 -i lan1 merlo 192.168.1.48/24
-pipework lan1 -i lan1 potrero 192.168.1.1/24
-pipework lan2 -i lan2 potrero 172.16.4.1/23
-pipework ppp1 -i ppp1 potrero 200.8.4.18/30
-pipework ppp1 -i ppp1 laflorida 200.8.4.17/30
-pipework man1 -i man1 laflorida 8.8.8.14/28
-pipework man1 -i man1 nogoli 8.8.8.8/28
-pipework man1 -i man1 desaguadero 8.8.8.1/28
-pipework ppp2 -i ppp2 desaguadero 170.0.2.6/30
-pipework ppp2 -i ppp2 carrizal 170.0.2.5/30
-pipework lan3 -i lan3 carrizal 10.22.0.1/16
-pipework lan3 -i lan3 laslenias 10.22.0.150/16
-
 
 docker exec -it latoma ip ro add default via 172.16.4.1
 docker exec -it merlo ip ro add default via 192.168.1.1
@@ -116,15 +103,6 @@ docker exec -it nogoli ip ro add 10.22.0.0/16 via 8.8.8.1
 docker exec -it nogoli ip ro add 200.8.4.16/30 via 8.8.8.14
 docker exec -it nogoli ip ro add 170.0.2.4/30 via 8.8.8.1
 
-xterm -T "latoma" -fa monaco -fs 11 -e "docker attach latoma" &
-xterm -T "merlo" -fa monaco -fs 11 -e "docker attach merlo" &
-xterm -T "potrero" -fa monaco -fs 11 -e "docker attach potrero" &
-xterm -T "laflorida" -fa monaco -fs 11 -e "docker attach laflorida" &
-xterm -T "desaguadero" -fa monaco -fs 11 -e "docker attach desaguadero" &
-xterm -T "nogoli" -fa monaco -fs 11 -e "docker attach nogoli" &
-xterm -T "carrizal" -fa monaco -fs 11 -e "docker attach carrizal" &
-xterm -T "laslenias" -fa monaco -fs 11 -e "docker attach laslenias" &
-
-
 #para saber los nombres de los contenedores que estan corriendo
 # docker ps  --format "table {{.Names}}"
+
